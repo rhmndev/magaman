8875c8de170430388c26f83487c1fdf8a9ab7f6c
hi-rahman <rahmannurhadi75@gmail.com>
2024-10-31 01:10:21 +0700

    feat: add tampaling engine blade for view

---
+++ app/Http/Controllers/FrontController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\ArticleNews;
+use App\Models\Author;
+use App\Models\Category;
+use App\Models\BannerAdvertisement;
+use Illuminate\Http\Request;
+
+class FrontController extends Controller
+{
+    //
+    public function index() {
+        $categories = Category::all();
+
+        $articles = ArticleNews::with(['category'])
+        ->where('is_featured', 'not_featured')
+        ->latest()
+        ->take(3)
+        ->get();
+        
+        $featured_articles = ArticleNews::with(['category'])
+        ->where('is_featured', 'featured')
+        ->inRandomOrder()
+        ->take(3)
+        ->get();
+
+        $authors = Author::all();
+
+        $banner_ads = BannerAdvertisement::where('is_active', 'active')
+        ->where('type', 'banner')
+        ->inRandomOrder()
+        // ->take(1)
+        ->first();
+        
+
+        return view('front.index', compact('categories', 'articles', 'authors', 'featured_articles', 'banner_ads'));
+    }
+}
